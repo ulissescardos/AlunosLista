76bb14711c59cf1413ab2fa30f704450c7f6f8d3
aigen charge <aigen.charge@gmail.com>
2022-11-21 17:32:56 -0300

    read file

--- src/scripts/show_students.php
+++ src/scripts/show_students.php
@@ -1,5 +1,20 @@
 <?php
 
+try {
+    
+
+    $file = fopen($path, "r");
+
+    while(!feof($file)){
+
+        $line = fgets($file);
+
+        var_dump(explode("\n", $line));
+    }
 
 
 
+
+} catch (Exception $e) {
+    //throw $th;
+}
