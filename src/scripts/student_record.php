9c02969d61cd0264885832dab35c036b39ad379d
aigen charge <aigen.charge@gmail.com>
2022-11-21 17:11:12 -0300

    add try catch

--- src/scripts/student_record.php
+++ src/scripts/student_record.php
@@ -1,8 +1,6 @@
 <?php
 
-require "./aluno.php";
-
-$students = array();
+include "./student.php";
 
 $student = new Student();
 
@@ -13,54 +11,119 @@ foreach($_POST as $key => $value){
     $student->$key = $val;
 }
 
-$students += $student;
+$path = dirname(__FILE__) . "/students.txt";
 
-echo "
-<!DOCTYPE html>
-<html lang='pt-BR'>
-<head>
-    <meta charset='UTF-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Lista de Alunos</title>
-    <!--
-        Foi usada a biblioteca css Bulma, para usar alguns dos estilos
-        site: https://bulma.io
-    -->
-    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css'>
-    <link rel='stylesheet' href='../style.css'>
-</head>
-<body>
-
-    <nav class='navbar'>
-        <ul>
-            <li class='navbar-brand'>
-                <a href='../index.html'>
-                    <h2> SI401B </h2>
-                </a>
-            </li>
-
-            <li>
-                <a href=''>
-                    Alunos Cadastrados
-                </a>
-            </li>
-
-            <li>
-                <a href='../index.html'>
-                    Cadastrar novo aluno
-                </a>
-            </li>
-
-        </ul>
-    </nav>
-
-    <main>
-
-        <h1> Aluno Cadastrado com sucesso! </h1>
-
-    </main>
+try {
     
-</body>
-</html>
-";
+    $file = fopen($path, "a+") or die("Não consegui abrir o arquivo");
+
+    echo "
+    <!DOCTYPE html>
+    <html lang='pt-BR'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Lista de Alunos</title>
+        <!--
+            Foi usada a biblioteca css Bulma, para usar alguns dos estilos
+            site: https://bulma.io
+        -->
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css'>
+        <link rel='stylesheet' href='../style.css'>
+    </head>
+    <body>
+
+        <nav class='navbar'>
+            <ul>
+                <li class='navbar-brand'>
+                    <a href='../index.html'>
+                        <h2> SI401B </h2>
+                    </a>
+                </li>
+
+                <li>
+                    <a href=''>
+                        Alunos Cadastrados
+                    </a>
+                </li>
+
+                <li>
+                    <a href='../index.html'>
+                        Cadastrar novo aluno
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+
+        <main>
+
+            <h1> Aluno Cadastrado com sucesso! </h1>
+
+        </main>
+        
+    </body>
+    </html>
+    ";
+
+    var_dump($student);
+
+} catch (Exception $e) {
+    
+    $file = fopen($path, "a+") or die("Não consegui abrir o arquivo");
+
+    echo "
+    <!DOCTYPE html>
+    <html lang='pt-BR'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Lista de Alunos</title>
+        <!--
+            Foi usada a biblioteca css Bulma, para usar alguns dos estilos
+            site: https://bulma.io
+        -->
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css'>
+        <link rel='stylesheet' href='../style.css'>
+    </head>
+    <body>
+
+        <nav class='navbar'>
+            <ul>
+                <li class='navbar-brand'>
+                    <a href='../index.html'>
+                        <h2> SI401B </h2>
+                    </a>
+                </li>
+
+                <li>
+                    <a href=''>
+                        Alunos Cadastrados
+                    </a>
+                </li>
+
+                <li>
+                    <a href='../index.html'>
+                        Cadastrar novo aluno
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+
+        <main>
+
+            <h1> Não possível cadastrar o aluno! Tente novamente! </h1>
+
+        </main>
+        
+    </body>
+    </html>
+    ";
+
+    var_dump($student);
+
+
+}
