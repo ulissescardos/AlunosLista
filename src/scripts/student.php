d72595f4663f2a7728f7fe18e17ff57b07804fa3
aigen charge <aigen.charge@gmail.com>
2022-11-22 09:56:01 -0300

    add static method sort_instances in class

--- src/scripts/student.php
+++ src/scripts/student.php
@@ -2,6 +2,13 @@
 
 class Student{
 
+    public static function sort_instances($st1, $st2, $atribute_name)
+    {
+        if($st1->$atribute_name == $st2->$atribute_name){ return 0 ; }
+
+        return ($st1->$atribute_name < $st2->$atribute_name) ? -1 : 1;
+    }
+
     public $name;
     public $age;
     public $ra;
