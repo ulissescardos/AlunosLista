0131fdcdef26261dffdf439011c78d016ec36520
aigen charge <aigen.charge@gmail.com>
2022-11-22 10:27:30 -0300

    set var atribute_name

--- src/scripts/student.php
+++ src/scripts/student.php
@@ -2,8 +2,10 @@
 
 class Student{
 
-    public static function sort_instances($st1, $st2, $atribute_name)
+    public static function sort_instances($st1, $st2)
     {
+        $atribute_name = "ra";
+
         if($st1->$atribute_name == $st2->$atribute_name){ return 0 ; }
 
         return ($st1->$atribute_name < $st2->$atribute_name) ? -1 : 1;
