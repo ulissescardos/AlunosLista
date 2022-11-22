84be14af365bf783f829635748f8e855ffe49254
aigen charge <aigen.charge@gmail.com>
2022-11-22 08:37:37 -0300

    read lines in file

--- src/scripts/show_students.php
+++ src/scripts/show_students.php
@@ -1,20 +1,38 @@
 <?php
 
+$path = dirname(__FILE__) . "/students.txt";
+
+$students = array();
+
 try {
     
-
     $file = fopen($path, "r");
 
     while(!feof($file)){
 
         $line = fgets($file);
 
-        var_dump(explode("\n", $line));
+        echo "<pre></pre>";
+
+        $s = explode("\n", $line);
+
+        var_dump($s);
+
+        $students[] = unserialize($s[0]);
     }
-
-
-
 
 } catch (Exception $e) {
     //throw $th;
 }
+
+
+echo "<pre></pre>";
+
+var_dump($students);
+
+for($i = 0; $i < count($students); $i++){
+
+    echo "<p>${gettype($students[$i])}</p>";
+
+}
+
